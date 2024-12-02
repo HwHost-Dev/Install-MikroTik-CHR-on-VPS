eacb8570f3be2ecc29e3bd80397f70c5cb7da201
Arthur C. <63045074+ZeGamerc@users.noreply.github.com>
2024-12-02 23:22:40 +0100

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
+export PATH=$PATH:/usr/bin:/bin
 
 echo
 echo "=== hwhost.fr ==="
 echo "=== MikroTik Installer ==="
 echo
-sleep 3
 
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get -y install gzip
 
 # Retrieve the latest version dynamically
 latest_version=$(curl -s https://mikrotik.com/download | grep -oP 'chr-\K[0-9]+\.[0-9]+\.[0-9]+(?=\.img\.zip)' | sort -V | tail -n 1)
@@ -20,25 +22,37 @@ gunzip -c chr.img.zip > chr.img && \
 kpartx -av chr.img
 mount /dev/mapper/loop3p2 /mnt/ && \
 
-ADDRESS=$(ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1) && \
-GATEWAY=$(ip route list | grep default | cut -d' ' -f 3) && \
+# Find the primary network interface with an IP address
+interface=$(ip -o -4 addr show up primary scope global | awk '{print $2}' | head -n 1)
 
+# Retrieve network information
+ADDRESS=$(ip addr show $interface | grep global | cut -d' ' -f 6 | head -n 1)
+GATEWAY=$(ip route list | grep default | cut -d' ' -f 3)
+HOSTNAME=$(hostname)
+
+
+# Create autorun script
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
 /ip service disable telnet
+/system identity set name=$HOSTNAME
 /user set 0 name=root password=123456789
 " > /mnt/rw/autorun.scr && \
 
-umount /mnt && \
-STORAGE=$(lsblk | grep disk | awk '{print $1}' | head -n 1) && \
-echo STORAGE is $STORAGE && \
-ETH=$(ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p') && \
-echo ETH is $ETH && \
-echo ADDRESS is $ADDRESS && \
-echo GATEWAY is $GATEWAY && \
-sleep 5 && \
+# Unmount and clean up
+sudo umount /mnt/ && \
 
-dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
-echo "Ok, reboot" && \
-echo 1 > /proc/sys/kernel/sysrq && \
+# Sync and write image to disk
+echo u > /proc/sysrq-trigger && \
+sudo dd if=chr.img bs=1024 of=/dev/sda && \
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Waiting 5 seconds..." && \
+#Waiting
+end_time=$((SECONDS + 5))
+while [ $SECONDS -lt $end_time ]; do
+    :
+done && \
+
+echo "Reboot OS" && \
 echo b > /proc/sysrq-trigger
